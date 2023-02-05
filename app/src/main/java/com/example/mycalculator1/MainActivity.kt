7a25f1a13d9810d5320c40ca58d32a551385c0b4
sanathbhimsen <sanathbhimsen26@gmail.com>
2023-02-04 21:56:02 -0500

    Added my change

--- app/src/main/java/com/example/mycalculator1/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator1/MainActivity.kt
@@ -125,5 +125,26 @@ class MainActivity : AppCompatActivity() {
             }
             binding.TvResutWindow.setText(answer.toString())
         }
+
+        binding.BtnMultiple.setOnClickListener { view: View ->
+            input1 = binding.EtPlainTextInput1.text.toString()
+            input2 = binding.EtPlainTextInput2.text.toString()
+            var answer : String = CalculatorModel.product(input1, input2)
+            if (answer.equals("invalid")){
+
+                val snackBarView  = Snackbar.make(view,
+                        R.string.invalidInput,
+                        Snackbar.LENGTH_SHORT)
+                val view = snackBarView.view
+                val params = view.layoutParams as FrameLayout.LayoutParams
+                params.gravity = Gravity.TOP
+                view.layoutParams = params
+                snackBarView.show()
+            }
+            binding.TvResutWindow.setText(answer.toString())
+        }
+
+
+
     }
 }
